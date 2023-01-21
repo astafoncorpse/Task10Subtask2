87deb1357f21424cbcf583e3d2e6324f264b190e
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-01-21 09:39:12 +0300

    Добавьте файлы проекта.

---
+++ Task10Subtask2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Task10Subtask2;
+
+namespace Task10Subtask2
+{
+    public interface ICalculator
+    {
+        void Sum(int a, int b);
+    }
+
+    public class MyCalc : ICalculator
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine("Ответ: {0} ", a + b);
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Erorr(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Erorr(string message)
+        {
+            Console.WriteLine(message);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine(message);
+        }
+
+    }
+    public interface IWorker
+    {
+
+        void Work();
+    }
+
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main()
+        {
+            Logger = new Logger();
+
+            var worker1 = new Worker1(Logger);
+
+            worker1.Work();
+
+            ICalculator mycalc;
+            try
+            {
+                Console.WriteLine("Ввидите значение 1: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ввидите значение 2: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                mycalc = new MyCalc();
+                mycalc.Sum(a, b);
+            }
+            catch (FormatException)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Введенное значение не коректно!");
+            }
+            finally
+            {
+                Console.WriteLine("Калькулятор окончил работу");
+            }
+        }
+    }
+}
+
+
---
+++ Task10Subtask2/Worker1.cs
@@ -0,0 +1,23 @@
+﻿using Task10Subtask2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task10Subtask2
+{
+    public class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Калькулятор начал свою работу");
+            
+            
+        }
+    }
+}
